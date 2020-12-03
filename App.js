5e4365ff5a000e4e17c6d8074a8ed3b9c50baa34
bonhommeepargne <bonhomme.epargne@gmail.com>
2020-12-03 15:15:22 +0100

    Initial Init

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Index from './src/Index';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <StatusBar style="auto" /> */}
+      <Index />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'red'
+  },
+});
---
+++ src/components/MySharedComponent.jsx
@@ -0,0 +1,269 @@
+import React, { Component } from 'react';
+import { StyleSheet, Button, Text, View, Image, Alert } from 'react-native';
+import img1 from '../../assets/starsgrey.png';
+
+class MySharedComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            icons: [
+                require('../../assets/starsgrey.png'),
+                require('../../assets/starsgreyyellow.png'),
+                require('../../assets/dashboard.png')
+            ]
+        }
+    }
+
+    changeIcon() {
+        console.log("Notre state : ", this.state);
+        // let iconsTab =
+        this.setState((Oldstate) => {
+            return { icons: [Oldstate.icons[2], Oldstate.icons[1], Oldstate.icons[0]] }
+        });
+    }
+
+    render() {
+        console.log("render");
+        return (
+            <View style={styles.container}>
+                <View style={styles.containerIcons}>
+                    <Image
+                        source={this.state.icons[0]}
+                        style={styles.stretch}
+                    />
+                    <Image
+                        source={this.state.icons[1]}
+                        style={styles.stretch}
+                    />
+                    <Image
+                        source={this.state.icons[2]}
+                        style={styles.stretch}
+                    />
+                </View>
+                <Button
+                    title="Press me"
+                    onPress={() => { this.changeIcon() }}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        borderWidth: 1,
+        borderColor: 'green'
+    },
+    containerIcons: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        width: '60%',
+        height: '20%',
+        borderWidth: 1,
+        borderColor: 'blue'
+    },
+    stretch: {
+        height: '50%',
+        width: '20%',
+        resizeMode: 'contain',
+        borderWidth: 1,
+        borderColor: 'blue'
+    }
+});
+
+export default MySharedComponent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
